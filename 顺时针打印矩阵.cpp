49d4b1477cb2e2d7861c0728fb035afa4c982eb0
flypyCode <job_cunyipeng@163.com>
2018-05-01 22:53:46 +0800

    Create 顺时针打印矩阵.cpp

---
+++ 顺时针打印矩阵.cpp
@@ -0,0 +1,38 @@
+class Solution {    
+public:
+    vector<int> printMatrix(vector<vector<int> > matrix) {
+        vector<int> max_vec;
+        max_vec.clear();
+        int rows = matrix.size();
+        int columns = matrix[0].size();
+        int start = 0;
+        int endX;
+        int endY;
+        
+        // 输入的二维数组非法，返回空的数组
+        if (rows == 0 || columns == 0)  return max_vec;
+        while(rows > start * 2 && columns > start * 2){
+            
+           endX = rows - 1 - start;
+           endY = columns - 1 - start;
+        
+           for(int i = start; i <= endX; ++i)
+               max_vec.push_back(matrix[start][i]);
+        
+           if(start < endY)
+               for(int i = start + 1; i <= endY; ++i)
+                   max_vec.push_back(matrix[i][endX]);
+        
+           if(start < endX && start < endY)
+               for(int i = endX - 1; i >= start; --i)
+                   max_vec.push_back(matrix[endY][i]);
+         
+        
+          if(start < endX && start < endY - 1)
+               for(int i = endY - 1; i >= start + 1; --i)
+                   max_vec.push_back(matrix[i][start]);
+          ++start;
+        }
+        return max_vec;
+    }
+};
