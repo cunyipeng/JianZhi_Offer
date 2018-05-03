37332cdb7fde53da5b5cc8addc2d54c96377c9d7
flypyCode <job_cunyipeng@163.com>
2018-05-03 09:34:15 +0800

    Create 链表中环的入口结点.cpp

---
+++ 链表中环的入口结点.cpp
@@ -0,0 +1,56 @@
+/*
+struct ListNode {
+    int val;
+    struct ListNode *next;
+    ListNode(int x) :
+        val(x), next(NULL) {
+    }
+};
+*/
+class Solution {
+    public:
+    ListNode* EntryNodeOfLoop(ListNode* pHead){
+        if(pHead == NULL|| pHead->next == NULL|| pHead->next->next == NULL)
+            return NULL;
+ 
+        ListNode* fast = pHead->next->next;
+ 
+        ListNode* slow = pHead->next;
+ 
+        /////先判断有没有环
+ 
+        while(fast != slow){
+ 
+            if(fast->next != NULL && fast->next->next != NULL){
+ 
+                fast = fast->next->next;
+ 
+                slow = slow->next;
+ 
+            }else{
+ 
+                //没有环,返回
+ 
+                return NULL;
+ 
+            }
+ 
+        }
+ 
+        //循环出来的话就是有环，且此时fast==slow.
+ 
+        fast = pHead;
+ 
+        while(fast != slow){
+ 
+            fast = fast->next;
+ 
+            slow = slow->next;
+ 
+        }
+ 
+        return slow;
+ 
+    }
+ 
+};
