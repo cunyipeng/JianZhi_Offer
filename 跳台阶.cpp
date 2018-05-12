c800812e238a8f67279660a8a7bd4d947ca9b970
flypyCode <job_cunyipeng@163.com>
2018-05-12 17:24:07 +0800

    Update 跳台阶.cpp

--- 跳台阶.cpp
+++ 跳台阶.cpp
@@ -26,3 +26,16 @@ public:
         return f;
     }
 };
+
+
+/*dp是全局数组，大小为n, 全部初始化为0，是题目中的动态规划表*/  
+int fun(int n){  
+    if (n==1||n==2)  
+        return n;  
+    /*判断n-1的状态有没有被计算过*/  
+    if (!dp[n-1])  
+        dp[n-1] = fun(n-1);  
+    if(!dp[n-2])  
+        dp[n-2]=fun(n-2);  
+    return dp[n-1]+dp[n-2];  
+}  
