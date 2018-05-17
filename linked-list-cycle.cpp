4c71e70786079424dd82a2a80b1939692c068d18
flypyCode <job_cunyipeng@163.com>
2018-05-17 11:48:10 +0800

    Create linked-list-cycle.cpp

---
+++ linked-list-cycle.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+ 
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if(head == NULL || head->next == NULL || head->next->next == NULL)
+            return false;
+        
+        ListNode *fast = head->next->next;
+        ListNode *slow = head->next;
+        
+        while(fast != slow){
+            if(fast->next != NULL && fast->next->next != NULL){
+                fast = fast->next->next;
+                slow = slow->next;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+};
