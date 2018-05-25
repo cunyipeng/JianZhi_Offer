eb6aec096b0ded19efaa8df39e648e6d9d5a3f60
flypyCode <job_cunyipeng@163.com>
2018-05-25 22:28:37 +0800

    Update KMP.cpp

--- KMP.cpp
+++ KMP.cpp
@@ -13,13 +13,13 @@ Status SetString(String T,char *chars)
 {
 	int i;
 	if(strlen(chars)>MAXSIZE)
-		return ERROR;
+	     return ERROR;
 	else
 	{
-		T[0]=strlen(chars);
-		for(i=1;i<=T[0];i++)
-			T[i]=*(chars+i-1);
-		return OK;
+	     T[0]=strlen(chars);
+	     for(i=1;i<=T[0];i++)
+		 T[i]=*(chars+i-1);
+	     return OK;
 	}
 }
 
@@ -31,14 +31,14 @@ void get_next(String T,  int *next)
   	next[1]=0;
   	while (i<T[0])
  	{
-    	if(j==0 || T[i]== T[j])
-		{
+    	   if(j==0 || T[i]== T[j])
+	   {
       		++i;
-			++j;
-			next[i] = j;
-    	}
-		else
-			j= next[j];
+		++j;
+		next[i] = j;
+    	   }
+	   else
+		j= next[j];
   	}
 }
 
@@ -50,29 +50,29 @@ int KMP(String S,  String T,  int pos)
 	get_next(T,  next);
 	while (i <= S[0] && j <= T[0])
 	{
-		if (j==0 || S[i] == T[j])
-      	{
+	   if (j==0 || S[i] == T[j])
+      	   {
          	++i;
          	++j;
-      	}
-      	else
+      	   }
+      	   else
       	 	j = next[j];
 	}
 	if (j > T[0])
-		return i - T[0];
+	   return i - T[0];
 	else
-		return 0;
+	   return 0;
 }
 
 int main()
 {
-	String S, L, s, l;
-	printf("请输入主串S:\n");
+    String S, L, s, l;
+    printf("请输入主串S:\n");
     scanf("%s",s);
     SetString(S, s);
     printf("请输入子串L:\n");
     scanf("%s", l);
     SetString (L, l);
     printf("\n%s在%s的第%d的位置\n", l, s, KMP(S, L, 1));
-	return 0;
+    return 0;
 }
